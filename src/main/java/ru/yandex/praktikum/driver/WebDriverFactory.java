4a85927f6ded6c4e17abf3a71b5450dfacb9febb
kristinarudik <kristina.rudik@mvideo.ru>
2024-02-14 10:16:02 +0300

    fix webdriver

--- src/main/java/ru/yandex/praktikum/driver/WebDriverFactory.java
+++ src/main/java/ru/yandex/praktikum/driver/WebDriverFactory.java
@@ -1,6 +1,5 @@
 package ru.yandex.praktikum.driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,12 +9,11 @@ public class WebDriverFactory {
 		ChromeOptions options = new ChromeOptions();
 		switch (System.getProperty("browser")) {
 			case "yandex":
-				options.setBinary("/Applications/Yandex.app/Contents/MacOS/Yandex");
-				System.setProperty("webdriver.chrome.driver","/Users/kristinarudik/IdeaProjects/Diplom_3/src/main/resources/yandexdriver");
+				System.setProperty("webdriver.chrome.driver","src/main/resources/yandexdriver");
 				options.addArguments("--remote-allow-origins=*");
 				return new ChromeDriver(options);
 			case "chrome":
-				System.setProperty("webdriver.chrome.driver","/Users/kristinarudik/IdeaProjects/Diplom_3/src/main/resources/chromedriver");
+				System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
 				options.addArguments("--remote-allow-origins=*");
 				return new ChromeDriver(options);
 			default:
